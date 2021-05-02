3db1af902df5ece28a453142a144ce333a3e9b6f
Amit kumar <50805574+Rohitkr786@users.noreply.github.com>
2021-05-02 16:51:50 +0530

    Add files via upload

---
+++ Martial-Arts-main/line.js
@@ -0,0 +1,76 @@
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+let loadImage = (src, callback) => {
+    let img = document.createElement("img");
+    img.onload = () => callback(img);
+    img.src = src;
+};
+
+let imagePath = (frameNumber, animation) => {
+    return "images/" + animation + "/" + frameNumber + ".png";
+};
+
+let frames = {
+    idle: [1, 2, 3, 4, 5, 6, 7, 8],
+    kick: [1, 2, 3, 4, 5, 6, 7],
+    punch: [1, 2, 3, 4, 5, 6, 7],
+};
+
+let loadImages = (callback) => {
+    let images = {
+        idle: [],
+        kick: [],
+        punch: [], 
+    }; 
+    let imagesToLoad = 0;
+
+    ["idle", "kick", "punch"].forEach((animation) => {
+        let animationFrames =frames[animation];
+        imagesToLoad = imagesToLoad + animationFrames.length;
+
+        animationFrames.forEach((frameNumber) => {
+            let path = imagePath(frameNumber, animation);
+
+            loadImage(path, (image) => {
+                 images[animation][frameNumber - 1]= image;
+                imagesToLoad = imagesToLoad - 1;
+
+                if( imagesToLoad === 0) {
+                    callback(images);
+                 }
+             });
+        });        
+    });
+};
+
+let animate = (ctx, images, animation, callback) => {
+    images[animation].forEach((image, index) => {
+        setTimeout(() => {
+            ctx.clearRect(0, 0, 500, 500);
+            ctx.drawImage(image, 0, 0, 500, 500);
+        }, index * 100);
+    });
+
+    setTimeout(callback, images[animation].length * 100);
+};
+
+loadImages((images) => { 
+    let queuedAnimation =[];
+    let aux = () => {
+        let selectedAnimation = queuedAnimation.length ===0 ? "idle" : queuedAnimation.shift();
+        animate(ctx, images, selectedAnimation, aux);
+    };
+
+    document.getElementById("kick").onclick = () => {
+        queuedAnimation.push("kick");
+    };
+
+    document.getElementById("punch").onclick = () => {
+        queuedAnimation.push("punch");
+    };
+    aux();
+});
+
+
+
